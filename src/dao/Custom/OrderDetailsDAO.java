39f195200e4663479e291f53e4a9d9bd8800a755
Amil Srinath <amilsrinath5@gmail.com>
2023-05-31 08:36:12 +0530

    OrderDetailsDAO Update

--- src/dao/Custom/OrderDetailsDAO.java
+++ src/dao/Custom/OrderDetailsDAO.java
@@ -9,6 +9,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public interface OrderDetailsDAO extends CrudDAO<OrderDetailsDAO,String> {
+public interface OrderDetailsDAO extends CrudDAO<OrderDetailDTO,String> {
     public boolean saveOrderDetails(OrderDetailDTO dto) throws SQLException, ClassNotFoundException;
 }
