db28d0d6beeeae3946d68a4f2bafa48439f59bc0
Amil Srinath <amilsrinath5@gmail.com>
2023-05-31 08:36:54 +0530

    PlaceOrderBO Update

--- src/bo/Custom/PlaceOrderBO.java
+++ src/bo/Custom/PlaceOrderBO.java
@@ -1,11 +1,31 @@
 package bo.Custom;
 
+import model.CustomerDTO;
 import model.ItemDTO;
 import model.OrderDTO;
+import model.OrderDetailDTO;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
-public interface PlaceOrderBO {
-    //-------
+public interface PlaceOrderBO extends SuperBO{
+    public CustomerDTO searchCustomer(String id) throws SQLException, ClassNotFoundException ;
+
+    public ItemDTO searchItem(String code) throws SQLException, ClassNotFoundException ;
+
+    public boolean existItem(String code) throws SQLException, ClassNotFoundException;
+
+    public boolean existCustomer(String id) throws SQLException, ClassNotFoundException ;
+
+    public String generateOrderID() throws SQLException, ClassNotFoundException ;
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException;
+
+    public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException;
+
+    public boolean purchaseOrder(String orderId, LocalDate orderDate, String customerId, List<OrderDetailDTO> orderDetails);
+
+    public ItemDTO findItem(String code);
 }
